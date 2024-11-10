801d8c14c172ff09268c4ca4b7ec0144a82929d5
John Kamel <seerpout@gmail.com>
2024-11-11 00:24:38 +0300

    I hate i18n

---
+++ 08_I18n/guesser.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+
+int main() {
+    setlocale (LC_ALL, "");
+    bindtextdomain ("_", getenv("PWD"));
+    textdomain ("_");
+
+    int low = 1, high = 100;
+    printf(_("Guess a number between %d and %d\n"), low, high);
+    printf(_("...press any to continue...\n"));
+    getchar();
+
+    char didIGuess = 'l';
+    int number;
+    while (didIGuess != 'e') {
+        int prevNumber = number;
+        number = (high + low - 1) / 2;
+
+        if (prevNumber == number) {
+            printf(_("You are lying, it has to be %d!\nI'm not playing w/ you >:(\n"), number);
+            exit(1);
+        }
+
+        printf(_("Is it %d? [l/e/g] "), number);
+        scanf("%c", &didIGuess);
+
+        while (didIGuess != 'l' && didIGuess != 'e' && didIGuess != 'g') {
+            if (didIGuess != '\n') {
+                printf("[y/n] ");
+            }
+            scanf("%c", &didIGuess);
+        }
+
+        if (didIGuess == 'l') {
+            high = number;
+        }
+        else {
+            low = number;
+        }
+    }
+
+    printf(_("I knew it was %d!\n"), number);
+
+    return 0;
+}
